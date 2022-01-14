fd388a0263346a267f9c0a3d16d493c4219e88d6
sercan-works <sercantas1026@gmail.com>
2022-01-14 13:44:23 +0300

    ss

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeCVBcXciBKWHXFP10X_r0kP3WYl75E4s",
-  authDomain: "fire-blog-90774.firebaseapp.com",
-  projectId: "fire-blog-90774",
-  storageBucket: "fire-blog-90774.appspot.com",
-  messagingSenderId: "468048236462",
-  appId: "1:468048236462:web:69c84719befbd37291423f"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL:process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 
